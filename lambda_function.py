db56cbddb8af60128a1dd29d8885b97c74289d07
kchinna <kanishkchinna@gmail.com>
2022-06-28 15:58:58 -0700

    more informative success message

--- lambda_function.py
+++ lambda_function.py
@@ -45,5 +45,5 @@ def lambda_handler(event, context):
         
     return {
         'statusCode': 200,
-        'body': json.dumps('Success')
+        'body': json.dumps('Successfully read .csv file and updated DynamoDB databse')
     }
