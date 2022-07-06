7fb07e5cbf46e593016a3e8c4384262f09b07885
kchinna <kanishkchinna@gmail.com>
2022-07-05 23:42:31 -0700

    added comment

--- lambda_function.py
+++ lambda_function.py
@@ -18,12 +18,13 @@ def lambda_handler(event, context):
         entries = file['Body'].read().decode('utf-8').split('\n')
         read_file = csv.reader(entries, delimiter = ',', quotechar = '"')
         
+        # Loop through rows in the provided .csv file
         for row in read_file:
             id = row[0]
             first_name = row[1]
             last_name = row[2]
             email = row[3]
-            password = hex(random.randrange(0, 10000))
+            password = hex(random.randrange(0, 100000))
             
             try:
                 add = database.put_item(
